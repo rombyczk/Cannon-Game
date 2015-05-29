5d9996690024ba58aa84d4e398de9facc3fd44f5
rombyczk <rombyczk@student.pg.gda.pl>
2015-05-29 22:57:29 +0200

    Zrobiono interfejs okna

--- WindowsFormApplication1/Form1.h
+++ WindowsFormApplication1/Form1.h
@@ -8,6 +8,7 @@ namespace WindowsFormApplication1 {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+    #include "Cannon.h"
 
 	/// <summary>
 	/// Summary for Form1
@@ -35,10 +36,16 @@ namespace WindowsFormApplication1 {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^  button1;
-	protected:
-	private: System::Windows::Forms::Button^  button2;
 
+	protected:
+
+	private: Cannon cannon;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Label^  label4;
 	private:
 		/// <summary>
 		/// Required designer variable.
@@ -52,41 +59,108 @@ namespace WindowsFormApplication1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label1->Location = System::Drawing::Point(41, 32);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(113, 31);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Points :";
+			this->label1->Click += gcnew System::EventHandler(this, &Form1::label1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label2->Location = System::Drawing::Point(29, 80);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(126, 31);
+			this->label2->TabIndex = 0;
+			this->label2->Text = L"  Lifes   :";
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(13, 158);
+			this->button1->Font = (gcnew System::Drawing::Font(L"Modern No. 20", 27.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(47, 138);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"Left";
+			this->button1->Size = System::Drawing::Size(108, 77);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"<-";
 			this->button1->UseVisualStyleBackColor = true;
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(197, 158);
+			this->button2->Font = (gcnew System::Drawing::Font(L"Modern No. 20", 26.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button2->Location = System::Drawing::Point(188, 138);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->Size = System::Drawing::Size(108, 77);
 			this->button2->TabIndex = 1;
-			this->button2->Text = L"Right";
+			this->button2->Text = L"->";
 			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label3->Location = System::Drawing::Point(268, 32);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(30, 31);
+			this->label3->TabIndex = 0;
+			this->label3->Text = L"0";
+			this->label3->TextAlign = System::Drawing::ContentAlignment::MiddleRight;
+			this->label3->Click += gcnew System::EventHandler(this, &Form1::label1_Click);
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label4->Location = System::Drawing::Point(268, 80);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(30, 31);
+			this->label4->TabIndex = 0;
+			this->label4->Text = L"5";
+			this->label4->TextAlign = System::Drawing::ContentAlignment::MiddleRight;
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->ClientSize = System::Drawing::Size(368, 237);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
 			this->Name = L"Form1";
 			this->Text = L"Controls";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
+		void Game_Inits()
+		{}
 #pragma endregion
-	};
+
+	
+private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+}
+};
 }
 
